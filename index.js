775a22dcce86d69b0697c95c20edb92e382c043b
Isabella <wileyisabella@gmail.com>
2021-03-18 19:22:11 -0400

    completed!

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, str){
+    const res = arr.filter(name => name.toLowerCase() == str.toLowerCase())
+    return res
+}
+function fuzzyMatch(arr, str){
+    const filtah = arr.filter(name => name.toLowerCase().indexOf(str.toLowerCase()) == 0)
+    return filtah
+}
+function matchName(arr, str){
+    const filtaa = arr.filter(driver => driver["name"].toLowerCase() == str.toLowerCase())
+    return filtaa
+}
